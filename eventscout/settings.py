ae52f3d6723bc5f5ba7aecf2a2a5d5e2ba54be3f
Shane Feltman <shane.feltman@gmail.com>
2025-04-07 22:35:25 -0400

    added google maps

--- eventscout/settings.py
+++ eventscout/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "eventscout.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'eventscout' / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
