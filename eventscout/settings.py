8911de5862828569fae35d5db78bc1169d4d53ed
Shane Feltman <shane.feltman@gmail.com>
2025-04-08 20:14:31 -0400

    attempted to do something..

--- eventscout/settings.py
+++ eventscout/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'eventscout' / 'static',
+    BASE_DIR / 'home' / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
