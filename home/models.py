4c2782cbd59e21a5ba27be816641f174b55d93d3
Zhandhu <107268513+Zhandhu@users.noreply.github.com>
2025-04-09 23:05:54 -0400

    changed up the user model

--- home/models.py
+++ home/models.py
@@ -1,23 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class AttendeeUser(models.Model):
+class User(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True, default='default@example.com')
     password = models.CharField(max_length=255)
     pfp = models.ImageField(upload_to='profile_pics/')
-    def __str__(self):
-        return str(self.id) + ' - ' + self.first_name + '-' + self.last_name
-
-
-class OrganizerUser(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True, default='default@example.com')
-    password = models.CharField(max_length=255)
-    pfp = models.ImageField(upload_to='profile_pics/')
+    role = models.BooleanField()
+    age = models.IntegerField(max_length=255)
+    gender = models.CharField(max_length=255)
+    demographic = models.CharField(max_length=255)
+    
     def __str__(self):
         return str(self.id) + ' - ' + self.first_name + '-' + self.last_name
