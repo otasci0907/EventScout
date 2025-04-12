02ce9c8f63e5fd3cd3f137dda907dbf60210218a
otasci0907 <otasci0907@gmail.com>
2025-04-12 12:02:59 -0400

    checking out other branches

--- eventscout/locations/views.py
+++ eventscout/locations/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ipware import get_client_ip
+import requests
+import json
+import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -64,15 +67,33 @@ test_events_w_locations = [
 
 @csrf_exempt
 def locations(request):
-    data = {}
+    api_key = os.environ.get('GOOGLE_API_KEY')
+    data = {
+        'API_KEY': api_key,
+    }
     if request.method == 'POST':
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
         events_in_range = get_events_in_range(1.5, float(latitude), float(longitude))
         data = {
-            'events': events_in_range
+            'events': events_in_range,
         }
-    return render(request, 'test.html', data)
+        print(events_in_range)
+    return render(request, 'test.html', {'data': data})
 
 def get_events_in_range(max_dist, lat, long):
     return [event for event in test_events_w_locations if event['coordinates']['latitude'] <= lat + max_dist and event['coordinates']['latitude'] >= lat - max_dist and event['coordinates']['longitude'] <= long + max_dist and event['coordinates']['longitude'] >= long - max_dist]
+
+def get_directions(event, lat, long):
+    destination_coord = str(event["coordinates"]["latitude"]) + "," + str(event["coordinates"]["longitude"])
+    origin_coord = str(lat) + "," + str(long)
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin_coord + "&destination=" + destination_coord +"&key=" + api_key
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        for step in data['routes'][0]['legs'][0]['steps']:
+            print(step['html_instructions'])
+        return data
+    else:
+        print(None)
+        return None
