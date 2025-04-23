b7162aa2560ab7227fefab11ccb66a67da1886ea
Shane Feltman <shane.feltman@gmail.com>
2025-04-23 15:42:12 -0400

    new branch off main

--- eventscout/settings.py
+++ eventscout/settings.py
@@ -14,10 +14,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
